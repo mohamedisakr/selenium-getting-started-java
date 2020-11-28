1c911b50f0e87e5625df0a25d668ddf426967702
mohamedisakr <mohamedisakr@gmail.com>
2020-11-29 00:33:05 +0200

    managing dependencies and executables

--- src/test/java/testcases/TestBrowser.java
+++ src/test/java/testcases/TestBrowser.java
@@ -2,12 +2,46 @@ package testcases;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestBrowser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("http://google.com");
 	}
 
 }
+/*
+
+public class ChromeTest {
+
+	private WebDriver driver;
+
+	@BeforeClass
+	public static void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@Before
+	public void setupTest() {
+		driver = new ChromeDriver();
+	}
+
+	@After
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void test() {
+		// Your test code here
+	}
+
+}
+
+*/
