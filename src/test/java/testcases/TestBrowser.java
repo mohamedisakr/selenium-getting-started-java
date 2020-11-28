0a199dc20b00ba47136bf92528d2080e255903a5
mohamedisakr <mohamedisakr@gmail.com>
2020-11-28 16:15:16 +0200

    adding new test

--- src/test/java/testcases/TestBrowser.java
+++ src/test/java/testcases/TestBrowser.java
@@ -1,10 +1,13 @@
 package testcases;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestBrowser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://google.com");
 	}
 
 }
